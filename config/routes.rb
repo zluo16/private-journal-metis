f42746937a8d78ef542b80623392db9cd684a8ed
Mike <zluo@alumni.berklee.edu>
2018-10-17 10:51:16 -0400

    Make login the root route and add resources for users and journals in router

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'sessions#new'
+  post '/login', to: 'sessions#create'
+  post '/logout', to: 'sessions#destroy'
+
+  resources :users, only: [:new, :create]
+  resources :journals
 end
